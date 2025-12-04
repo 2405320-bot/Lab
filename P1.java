331fc8c36ae8bdd99c44f04661a1b9637854c229
2405320 <2405320@kiit.ac.in>
2025-12-04 17:26:05 +0530

    this is something

---
+++ P1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;   //package used to use scanner object
+public class P1 {
+public static void main (String[] args){
+// calculate the area of rectangle
+System.out.println("Hello World!");
+}
+}
